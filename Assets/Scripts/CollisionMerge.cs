469c56e67c63d1433240e3d98b4b9f5af8c0136f
Eric Rosen <133508381+eric-rosen@users.noreply.github.com>
2023-10-08 19:02:06 -0400

    collision check

---
+++ Assets/Scripts/CollisionMerge.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionMerge : MonoBehaviour
+{
+    public float threshold_magnitude = 2f;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (threshold_magnitude < collision.relativeVelocity.magnitude)
+        {
+            Debug.Log("Attached!");
+        }
+        else
+        {
+            Debug.Log("Not hard enough...");
+        }
+    }
+}
